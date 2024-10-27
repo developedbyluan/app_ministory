777d8946f10f9a0405e82dfbd3f9c45dde0fda9a
Luan Tran <developedbyluan@gmail.com>
2024-10-27 21:41:34 +0700

    Create SplashScreen component

---
+++ components/SplashScreen/index.tsx
@@ -0,0 +1,13 @@
+import LoadingLogo from "@/components/SplashScreen/LoadingLogo";
+import WelcomeText from "@/components/SplashScreen/WelcomeText";
+import EnterAppButton from "@/components/SplashScreen/EnterAppButton";
+
+export default function SplashScreen({ hrefValue }: { hrefValue: string }) {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white p-4 overflow-hidden">
+      <LoadingLogo />
+      <WelcomeText />
+      <EnterAppButton hrefValue={hrefValue} />
+    </div>
+  );
+}
