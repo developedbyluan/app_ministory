e44c6eed8deed9991898ddc365e6246a0aad7405
Luan Tran <developedbyluan@gmail.com>
2024-10-27 21:43:47 +0700

    Import SplashScreen component to HomePage and render it

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import SplashScreen from "@/components/SplashScreen";
+
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Mini Story App</h1>
-    </div>
-  );
+  return <SplashScreen hrefValue="/" />;
 }
