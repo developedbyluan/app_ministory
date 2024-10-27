c2488c915ea51dd24be6dec752b61d7e59ad5b6d
Luan Tran <developedbyluan@gmail.com>
2024-10-27 21:24:35 +0700

    Modify WelcomeText: Change Title and Subtitle styles and contents

--- components/SplashScreen/WelcomeText.tsx
+++ components/SplashScreen/WelcomeText.tsx
@@ -10,17 +10,17 @@ export default function WelcomeText() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, delay: 0.5 }}
       >
-        Mini Story App
+        MINI STORY APP
       </motion.h1>
 
       <motion.p
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, delay: 0.8 }}
-        style={{ marginTop: "0.125rem" }}
+        style={{ marginTop: "0.25rem" }}
         className="mb-8 text-center text-lg"
       >
-        Learn Faster, Remember Longer
+        Learn Languages Effortlessly
       </motion.p>
     </>
   );
