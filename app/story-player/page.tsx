b3fcc08ac0f23e4b5951e03df56f75f7309536bc
Luan Tran <developedbyluan@gmail.com>
2024-10-30 00:20:55 +0700

    Get a specific mp3 File from IndexedDB and load it to an audio element

--- app/story-player/page.tsx
+++ app/story-player/page.tsx
@@ -1,17 +1,29 @@
 "use client";
 
-import { useDataContext } from "@/contexts/DataContext";
+// import { useDataContext } from "@/contexts/DataContext";
 import { useEffect, useState } from "react";
+import { get } from "idb-keyval";
 
 export default function StoryPlayerPage() {
-  const { audioFile } = useDataContext();
+  // const { audioFile } = useDataContext();
   const [audioUrl, setAudioUrl] = useState<string | null>(null);
 
-  useEffect(() => {
-    if (audioFile) {
-      setAudioUrl(URL.createObjectURL(audioFile));
-    }
-  }, [audioFile]);
+  // useEffect(() => {
+  //   if (audioFile) {
+  //     setAudioUrl(URL.createObjectURL(audioFile));
+  //   }
+  // }, [audioFile]);
 
-  return <div>{audioUrl && <audio src={audioUrl} controls />}</div>;
+  useEffect(() => {
+    get("Day of the Dead MS.mp3").then((file) => {
+      setAudioUrl(URL.createObjectURL(file));
+    });
+  }, []);
+
+  return (
+    <div>
+      {/* Using ContextAPI: {audioUrl && <audio src={audioUrl} controls />} */}
+      <div>Using IDB: {audioUrl && <audio src={audioUrl} controls />}</div>
+    </div>
+  );
 }
