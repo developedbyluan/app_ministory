8bc63ca155aff02139442f58611cb98e730fe061
Luan Tran <developedbyluan@gmail.com>
2024-11-01 08:35:53 +0700

    Add audioRef and currentTime state to AllLyricsPlayer
    - Add currentTime state to track the audio's current playback time.
    - Implement useEffect to add and clean up a timeupdate event listener on the audio element.
    - Update JSX to include audioRef in the audio element for proper event handling.

--- components/AllLyricsPlayer/index.tsx
+++ components/AllLyricsPlayer/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useDataContext } from "@/contexts/DataContext";
 import { createStore, get } from "idb-keyval";
 import LyricsDisplay from "./LyricsDisplay";
@@ -11,6 +11,9 @@ import { Lyric } from "@/types/types";
 export default function AllLyricsPlayer({ audioKey }: { audioKey: string }) {
   const [audioUrl, setAudioUrl] = useState<string | null>(null);
   const { audioFile } = useDataContext();
+
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [currentTime, setCurrentTime] = useState<number>(0);
 
   const lyrics: Lyric[] | undefined = ministoryDB.get(audioKey);
 
@@ -32,14 +35,25 @@ export default function AllLyricsPlayer({ audioKey }: { audioKey: string }) {
     });
   }, [audioFile, audioKey]);
 
+  useEffect(() => {
+    const audio = audioRef.current;
+    if (!audio) return;
+    console.log("Adding timeupdate event listener");
+    const updateTime = () => setCurrentTime(audio.currentTime);
+
+    audio.addEventListener("timeupdate", updateTime);
+
+    return () => audio.removeEventListener("timeupdate", updateTime);
+  }, [audioUrl]);
+
   return (
-    audioUrl &&
-    audioKey &&
-    lyrics && (
-      <div>
-        <audio src={audioUrl} controls />
-        <LyricsDisplay lyrics={lyrics} />
-      </div>
-    )
+    <div>
+      {audioUrl && audioKey && lyrics && (
+        <div>
+          <audio ref={audioRef} src={audioUrl} controls />
+          <LyricsDisplay lyrics={lyrics} />
+        </div>
+      )}
+    </div>
   );
 }
