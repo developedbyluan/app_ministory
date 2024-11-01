1cbf224b14c7353f151bad3292f3805b090acaed
Luan Tran <developedbyluan@gmail.com>
2024-11-02 06:40:52 +0700

    Replaced native <button> elements with the custom <Button> component from "@/components/ui/button".

--- components/AllLyricsPlayer/AudioControls.tsx
+++ components/AllLyricsPlayer/AudioControls.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 type AudioControlsProps = {
   isReplaying: boolean;
   isPlaying: boolean;
@@ -16,25 +18,26 @@ export default function AudioControls({
   return (
     <div className="w-full bg-zinc-50/80 fixed bottom-0 flex justify-between py-4 px-4">
       {!isReplaying ? (
-        <button
+        <Button
           disabled={isReplaying}
           className="text-zinc-50 bg-zinc-950 px-4 py-2 rounded-md text-lg"
           onClick={togglePlayPause}
         >
           {isPlaying ? "Pause" : "Play"}
-        </button>
+        </Button>
       ) : (
         <p className="w-full text-zinc-950 px-4 py-2 font-bold text-center text-xl">
           Replaying...
         </p>
       )}
       {!isReplaying && !isPlaying && (
-        <button
-          className="text-zinc-50 bg-zinc-950 px-4 py-2 rounded-md text-lg"
+        <Button
+          variant="ghost"
+          className="font-extrabold text-2xl"
           onClick={changePlaybackRate}
         >
           {playbackRate}x
-        </button>
+        </Button>
       )}
     </div>
   );
