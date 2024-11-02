4f57b6d37bf38bfcff051414c05385875264f32e
Luan Tran <developedbyluan@gmail.com>
2024-11-03 06:38:03 +0700

    Modify PlayPauseToggler: Remove play/pause text

--- components/AllLyricsPlayer/PlayPauseToggler.tsx
+++ components/AllLyricsPlayer/PlayPauseToggler.tsx
@@ -18,13 +18,12 @@ export default function PlayPauseToggler({
       disabled={isReplaying}
       onClick={togglePlayPause}
       aria-label={playPauseAriaLabel}
-      className={`flex gap-2 justify-center items-center px-4 py-2 rounded-full transition-all duration-300 ease-in-out border-2 ${
+      className={`flex gap-2 justify-center items-center p-2 rounded-full transition-all duration-300 ease-in-out border-2 ${
         isPlaying
           ? "bg-black text-white shadow-inner font-bold text-xl border-white"
           : "bg-white text-black font-bold text-xl shadow-[0_0_0_2px_inset_rgba(0,0,0,0.1),0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-2px_rgba(0,0,0,0.1)] "
       }`}
     >
-      {isPlaying ? "Pause" : "Play"}
       {isPlaying ? <Pause /> : <Play />}
     </button>
   );
