40639c557b75365beed5110d7dc19ad5e38f4d36
Luan Tran <developedbyluan@gmail.com>
2024-11-03 06:38:49 +0700

    Modify TranslationToggler: Add border when showTranslation being false

--- components/AllLyricsPlayer/TranslationToggle.tsx
+++ components/AllLyricsPlayer/TranslationToggle.tsx
@@ -23,10 +23,10 @@ export default function TranslationToggle({
       />
       <label
         htmlFor="toggle-translation-checkbox"
-        className={`flex items-center justify-center px-2 py-1 rounded-md font-extrabold text-xl cursor-pointer transition-colors duration-200 ease-in-out ${
+        className={`flex items-center justify-center px-2 py-1 rounded-md font-extrabold text-sm cursor-pointer border transition-colors duration-200 ease-in-out ${
           showTranslation
-            ? "bg-black text-white border border-white"
-            : "bg-black text-zinc-600"
+            ? "bg-black text-white border-white"
+            : "bg-zinc-950 text-zinc-600 border-zinc-600"
         }`}
       >
         Vn
