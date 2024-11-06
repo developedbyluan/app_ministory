5e725615d171422540000184bd78edde1a0db752
Luan Tran <developedbyluan@gmail.com>
2024-11-06 20:21:38 +0700

    Handle edge case: Hide AutoPauseMode controls while audio replaying

--- components/AutoPauseMode/index.tsx
+++ components/AutoPauseMode/index.tsx
@@ -62,45 +62,54 @@ export default function AutoPauseMode({
           <p>No explanationary found</p>
         )}
       </div>
-      <div className="w-full bg-zinc-950 p-4 rounded-tl-xl rounded-tr-xl fixed bottom-0 left-0 right-0 flex items-center justify-between">
-        <button
-          className="bg-zinc-800 p-2 rounded-full hover:bg-zinc-700 transition-colors duration-300"
-          onClick={() => {
-            setShowAutoPauseMode(false);
-            setScrollLyricIntoView(true);
-          }}
-        >
-          <X />
-        </button>
+      <div className="w-full bg-zinc-950 p-4 rounded-tl-xl rounded-tr-xl fixed bottom-0 left-0 right-0">
+        {!isReplaying ? (
+          <div
+            id="auto-pause-mode-controls"
+            className="flex items-center justify-between"
+          >
+            <button
+              className="bg-zinc-800 p-2 rounded-full hover:bg-zinc-700 transition-colors duration-300"
+              onClick={() => {
+                setShowAutoPauseMode(false);
+                setScrollLyricIntoView(true);
+              }}
+            >
+              <X />
+            </button>
 
-        <button
-          className="bg-zinc-800 p-2 rounded-full hover:bg-zinc-700 transition-colors duration-300"
-          onClick={stepBack}
-        >
-          <StepBack />
-        </button>
-        <button
-          className="bg-zinc-800 p-2 rounded-full hover:bg-zinc-700 transition-colors duration-300"
-          onClick={stepForward}
-        >
-          <StepForward size={36} />
-        </button>
-        <button className="bg-zinc-800 p-2 rounded-full hover:bg-zinc-700 transition-colors duration-300">
-          <Languages />
-        </button>
-        <button
-          className="bg-zinc-800 p-2 rounded-full hover:bg-zinc-700 transition-colors duration-300"
-          onClick={() => {
-            if (!lyrics) return;
-            handleLyricClick(
-              currentExplanationaryIndex,
-              lyrics[currentExplanationaryIndex].startTime,
-              lyrics[currentExplanationaryIndex].endTime
-            );
-          }}
-        >
-          <Repeat1 />
-        </button>
+            <button
+              className="bg-zinc-800 p-2 rounded-full hover:bg-zinc-700 transition-colors duration-300"
+              onClick={stepBack}
+            >
+              <StepBack />
+            </button>
+            <button
+              className="bg-zinc-800 p-2 rounded-full hover:bg-zinc-700 transition-colors duration-300"
+              onClick={stepForward}
+            >
+              <StepForward size={36} />
+            </button>
+            <button className="bg-zinc-800 p-2 rounded-full hover:bg-zinc-700 transition-colors duration-300">
+              <Languages />
+            </button>
+            <button
+              className="bg-zinc-800 p-2 rounded-full hover:bg-zinc-700 transition-colors duration-300"
+              onClick={() => {
+                if (!lyrics) return;
+                handleLyricClick(
+                  currentExplanationaryIndex,
+                  lyrics[currentExplanationaryIndex].startTime,
+                  lyrics[currentExplanationaryIndex].endTime
+                );
+              }}
+            >
+              <Repeat1 />
+            </button>
+          </div>
+        ) : (
+          <p className="text-center text-white">...</p>
+        )}
       </div>
     </div>
   );
