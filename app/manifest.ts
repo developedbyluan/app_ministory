bac35747bac148c7cf21a0d5a8d228df4537ea0c
Luan Tran <developedbyluan@gmail.com>
2024-11-08 18:07:52 +0700

    Manifest: Change bg color to white

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
       "Mini Story App is a simple and easy-to-use audio player app tailored for Effortless English Learners taught by A.J. Hoge.",
     start_url: "/",
     display: "standalone",
-    background_color: "#09090b",
+    background_color: "#ffffff",
     theme_color: "#09090b",
     orientation: "portrait-primary",
     icons: [
