9903608b18354c173c15900a8604e16600154689
Luan Tran <developedbyluan@gmail.com>
2024-11-08 08:45:40 +0700

    1. Create The Web App Manifest

---
+++ app/manifest.ts
@@ -0,0 +1,39 @@
+export default function manifest() {
+  return {
+    name: "Mini Story App",
+    short_name: "MiniStory",
+    description:
+      "Mini Story App is a simple and easy-to-use audio player app tailored for Effortless English Learners taught by A.J. Hoge.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ff0000",
+    theme_color: "#0000ff",
+    orientation: "portrait-primary",
+    icons: [
+      {
+        src: "/icons/manifest-icon-192.maskable.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icons/manifest-icon-192.maskable.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/icons/manifest-icon-512.maskable.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icons/manifest-icon-512.maskable.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  };
+}
