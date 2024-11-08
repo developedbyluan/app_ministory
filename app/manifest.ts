746bc730935cb30d094058a21d599d453b216de9
Luan Tran <developedbyluan@gmail.com>
2024-11-08 14:11:59 +0700

    Manifest: set bg and them colors to #09090b

--- app/manifest.ts
+++ app/manifest.ts
@@ -9,7 +9,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#09090b",
-    theme_color: "#ffffff",
+    theme_color: "#09090b",
     orientation: "portrait-primary",
     icons: [
       {
