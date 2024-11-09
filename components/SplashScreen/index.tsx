777b38deba74a56e5f403f1cb5c1477e800529fc
Luan Tran <developedbyluan@gmail.com>
2024-11-10 04:53:24 +0700

    SplashScreen component: disable redirect to /upload-audio

--- components/SplashScreen/index.tsx
+++ components/SplashScreen/index.tsx
@@ -1,11 +1,11 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
 import LoadingLogo from "@/components/SplashScreen/LoadingLogo";
 import WelcomeText from "@/components/SplashScreen/WelcomeText";
 import EnterAppButton from "@/components/SplashScreen/EnterAppButton";
 
 export default function SplashScreen({ hrefValue }: { hrefValue: string }) {
-  redirect("/upload-audio");
+  // redirect("/upload-audio");
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white p-4 overflow-hidden">
