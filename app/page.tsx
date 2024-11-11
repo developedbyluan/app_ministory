252961ce0aa8bba54eba31125658584fa0a0e4b6
Luan Tran <developedbyluan@gmail.com>
2024-11-11 09:30:49 +0700

    Move usePWA hook from HomePage to SplashScreen component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import SplashScreen from "@/components/SplashScreen";
-import usePWA from "@/hooks/use-pwa";
 
 export default function HomePage() {
-  const { isOnline } = usePWA();
-  return <SplashScreen hrefValue="/upload-audio" isOnline={isOnline} />;
+  return <SplashScreen hrefValue="/upload-audio" />;
 }
--- components/SplashScreen/index.tsx
+++ components/SplashScreen/index.tsx
@@ -4,15 +4,14 @@ import LoadingLogo from "@/components/SplashScreen/LoadingLogo";
 import WelcomeText from "@/components/SplashScreen/WelcomeText";
 import EnterAppButton from "@/components/SplashScreen/EnterAppButton";
 
+import usePWA from "@/hooks/use-pwa";
+
 type SplashScreenProps = {
   hrefValue: string;
-  isOnline: boolean;
 };
 
-export default function SplashScreen({
-  hrefValue,
-  isOnline,
-}: SplashScreenProps) {
+export default function SplashScreen({ hrefValue }: SplashScreenProps) {
+  const { isOnline } = usePWA();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white p-4 overflow-hidden">
       <LoadingLogo />
