9f31e024b9feab7932730060004fb2455c38cbf1
Luan Tran <developedbyluan@gmail.com>
2024-11-11 08:05:16 +0700

    fix: sw.js: set activate to wait until delete old cache versions, then finish
    also:
    - remove all console.log in sw.js

--- public/sw.js
+++ public/sw.js
@@ -4,31 +4,27 @@ self.addEventListener("install", (event) => {
   console.log("1. Service worker installing", event);
   event.waitUntil(
     caches.open(STATIC_CACHE_NAME).then((cache) => {
-      console.log("1.1. Caching static assets", cache);
       return cache.addAll(["/"]);
     })
   );
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("2. Service worker activating", event);
-  caches.keys().then((keys) => {
-    console.log("2.1. Service worker cache keys", keys);
-    return keys
-      .filter((key) => key !== STATIC_CACHE_NAME)
-      .map((key) => caches.delete(key));
-  });
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return keys
+        .filter((key) => key !== STATIC_CACHE_NAME)
+        .map((key) => caches.delete(key));
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("3. Service worker fetching", event);
   event.respondWith(
     caches.match(event.request).then((cacheResponse) => {
-      console.log("3.1. Service worker cache response", cacheResponse);
       return (
         cacheResponse ||
         fetch(event.request).catch(() => {
-          console.log("3.2. Service worker cache miss", event.request);
           return caches.match("/");
         })
       );
