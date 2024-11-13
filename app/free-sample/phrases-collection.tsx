459e1acff9d4292f0992ef9177d0c9a161f9d88e
Luan Tran <developedbyluan@gmail.com>
2024-11-13 22:11:24 +0700

    feat (/free-sample page): create phrases collection data

---
+++ app/free-sample/phrases-collection.tsx
@@ -0,0 +1,41 @@
+import { Phrases } from "@/types/types";
+
+export const phrasesCollection: Phrases[] = [
+  {
+    id: 0,
+    phrases: [
+      {
+        phrase: "Hello,",
+        ipa: "həˈloʊ,",
+        meaning: "Xin chào,",
+        explanation: "Một cách nói chào thân thiện khi gặp ai đó.",
+      },
+      {
+        phrase: "welcome to",
+        ipa: "ˈwɛlkəm tu",
+        meaning: "chào mừng bạn đến với",
+        explanation: "Cách nói mời ai đó vào nơi nào hoặc chào đón họ.",
+      },
+      {
+        phrase: "the mini-story",
+        ipa: "ðə ˈmɪni-ˈstɔri",
+        meaning: "câu chuyện ngắn",
+        explanation: "'mini' có nghĩa là 'nhỏ/ngắn', 'story' là 'câu chuyện'.",
+      },
+      {
+        phrase: "for The Race.",
+        ipa: "fɔr ðə reɪs.",
+        meaning: "về Cuộc Đua.",
+        explanation:
+          "Giới thiệu tên câu chuyện là 'Cuộc Đua'. 'for' chỉ mục đích hoặc đối tượng của câu chuyện.",
+      },
+      {
+        phrase: "Let’s get started.",
+        ipa: "lɛts gɛt ˈstɑrtəd.",
+        meaning: "Hãy bắt đầu nào.",
+        explanation:
+          "Diễn đạt lời mời cùng bắt đầu hoặc khởi đầu một việc gì đó.",
+      },
+    ],
+  },
+];
