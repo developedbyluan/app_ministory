f65ed32f01a33128c715e0cf7c385585b19f5d81
Luan Tran <developedbyluan@gmail.com>
2024-11-13 08:50:46 +0700

    refactor: extract StandardPlayer component from FreeSamplePage

--- app/free-sample/page.tsx
+++ app/free-sample/page.tsx
@@ -1,17 +1,12 @@
 "use client";
-// import { useState } from "react";
+
 import useAllLyricsPlayer from "@/hooks/use-all-lyrics-player";
-import AudioControlsV2 from "@/components/AudioControlsV2";
+import ProgressBar from "@/components/AllLyricsPlayer/ProgressBar";
+import StandardPlayer from "@/components/StandardPlayer";
 
 import { lyrics } from "./lyrics";
-import ProgressBar from "@/components/AllLyricsPlayer/ProgressBar";
-import LyricsDisplay from "@/components/AllLyricsPlayer/LyricsDisplay";
 
 export default function FreeSamplePage() {
-  // const [isPlaying, setIsPlaying] = useState(false);
-  // const [isReplaying, setIsReplaying] = useState(false);
-  // const [showTranslation, setShowTranslation] = useState(false);
-
   const {
     audioRef,
     audioUrl,
@@ -28,43 +23,21 @@ export default function FreeSamplePage() {
     lyrics: lyrics,
   });
 
-  // const handlePlayPause = () => {
-  //   setIsPlaying((prev) => !prev);
-  // };
-
-  // const handleReplay = () => {
-  //   setIsReplaying((prev) => !prev);
-  //   setTimeout(() => {
-  //     setIsReplaying(false);
-  //   }, 1000);
-  // };
-
-  // const handleShowTranslation = () => {
-  //   setShowTranslation((prev) => !prev);
-  // };
-
   return (
     <div>
       <ProgressBar progress={progress} />
       {audioUrl && <audio ref={audioRef} src={audioUrl} />}
 
-      <div id="standard-player" className="pt-10 px-7">
-        <LyricsDisplay
-          lyrics={lyrics}
-          onLyricClick={handleLyricClick}
-          activeLyricIndex={activeLyricIndex}
-          lyricRefsArray={lyricRefsArray}
-          isPlaying={isPlaying}
-          showTranslation={showTranslation}
-        />
-        <AudioControlsV2
-          isPlaying={isPlaying}
-          onPlayPause={togglePlayPause}
-          showTranslation={showTranslation}
-          onShowTranslation={() => setShowTranslation((prev) => !prev)}
-          type="standard"
-        />
-      </div>
+      <StandardPlayer
+        lyrics={lyrics}
+        handleLyricClick={handleLyricClick}
+        activeLyricIndex={activeLyricIndex}
+        lyricRefsArray={lyricRefsArray}
+        isPlaying={isPlaying}
+        showTranslation={showTranslation}
+        togglePlayPause={togglePlayPause}
+        setShowTranslation={setShowTranslation}
+      />
 
       <div id="auto-pause-player"></div>
       {/* <AudioControlsV2
---
+++ components/StandardPlayer.tsx
@@ -0,0 +1,46 @@
+import { Lyric } from "@/types/types";
+import LyricsDisplay from "./AllLyricsPlayer/LyricsDisplay";
+import AudioControlsV2 from "./AudioControlsV2";
+import { Dispatch, SetStateAction } from "react";
+
+type StandardPlayerProps = {
+  lyrics: Lyric[];
+  handleLyricClick: (clickedLyricIndex: number, startTime: number) => void;
+  activeLyricIndex: number;
+  lyricRefsArray: React.RefObject<HTMLParagraphElement[]>;
+  isPlaying: boolean;
+  showTranslation: boolean;
+  togglePlayPause: () => void;
+  setShowTranslation: Dispatch<SetStateAction<boolean>>;
+};
+
+export default function StandardPlayer({
+  lyrics,
+  handleLyricClick,
+  activeLyricIndex,
+  lyricRefsArray,
+  isPlaying,
+  showTranslation,
+  togglePlayPause,
+  setShowTranslation,
+}: StandardPlayerProps) {
+  return (
+    <div id="standard-player" className="pt-10 px-7">
+      <LyricsDisplay
+        lyrics={lyrics}
+        onLyricClick={handleLyricClick}
+        activeLyricIndex={activeLyricIndex}
+        lyricRefsArray={lyricRefsArray}
+        isPlaying={isPlaying}
+        showTranslation={showTranslation}
+      />
+      <AudioControlsV2
+        isPlaying={isPlaying}
+        onPlayPause={togglePlayPause}
+        showTranslation={showTranslation}
+        onShowTranslation={() => setShowTranslation((prev) => !prev)}
+        type="standard"
+      />
+    </div>
+  );
+}
