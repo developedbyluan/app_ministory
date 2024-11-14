d64a7bafa9bb1355ac87d6c11a26222a6817cdf3
Luan Tran <developedbyluan@gmail.com>
2024-11-14 11:37:37 +0700

    style (AutoPausePlayer): lower opacity of Close and Hide Buttons to 0.5

--- components/AudioControlsV2/HideButton.tsx
+++ components/AudioControlsV2/HideButton.tsx
@@ -12,7 +12,7 @@ export default function HideButton({
   return (
     <motion.button
       initial={{ y: -100, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
+      animate={{ y: 0, opacity: 0.5 }}
       exit={{ y: -100, opacity: 0 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
       className="fixed top-3 left-1 px-4 py-3 rounded-2xl bg-gradient-to-r from-zinc-700 to-zinc-600"
