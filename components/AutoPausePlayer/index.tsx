f8f6750d49d933e2f943cc766a7c9fd59b9d3e67
Luan Tran <developedbyluan@gmail.com>
2024-11-14 08:42:36 +0700

    feat: create AutoPausePlayer component for focused audio learning
    - Add component for single-phrase audio playback
    - Implement replay functionality with pause controls
    - Include translation toggle and phrase display

---
+++ components/AutoPausePlayer/index.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Dispatch, SetStateAction, useState } from "react";
+import PhrasePopovers from "./PhrasePopovers";
+import { phrasesCollection } from "@/app/free-sample/phrases-collection";
+import AudioControlsV2 from "../AudioControlsV2";
+import { Lyric } from "@/types/types";
+
+type AutoPausePlayerProps = {
+  lyrics: Lyric[];
+  activeLyricIndex: number;
+  handleReplay: (
+    lyricIndex: number,
+    startTime: number,
+    endTime: number
+  ) => void;
+  showTranslation: boolean;
+  setShowTranslation: Dispatch<SetStateAction<boolean>>;
+  isPlaying: boolean;
+  isReplaying: boolean;
+};
+
+export default function AutoPausePlayer({
+  lyrics,
+  activeLyricIndex,
+  handleReplay,
+  showTranslation,
+  setShowTranslation,
+  isPlaying,
+  isReplaying,
+}: AutoPausePlayerProps) {
+  const [currentIndex, setCurrentIndex] = useState(
+    activeLyricIndex > -1 ? activeLyricIndex : 1
+  );
+
+  function replaySingleLyric() {
+    if (currentIndex < lyrics.length - 1) {
+      handleReplay(
+        currentIndex,
+        lyrics[currentIndex].startTime,
+        lyrics[currentIndex].endTime
+      );
+    }
+  }
+  return (
+    <div className="fixed inset-0 z-40 bg-zinc-50 flex h-svh justify-center">
+      <div className="flex flex-col justify-center h-5/6 bg-red-100 mt-14 px-4">
+        <PhrasePopovers
+          sentenceAsPhrases={phrasesCollection[currentIndex]}
+          showIpa={false}
+        />
+        {showTranslation && (
+          <p className="text-gray-700 text-sm mx-1">
+            {lyrics[currentIndex].translation}
+          </p>
+        )}
+      </div>
+      <AudioControlsV2
+        type="auto-pause"
+        isPlaying={isPlaying}
+        isReplaying={isReplaying}
+        showTranslation={showTranslation}
+        onShowTranslation={() => setShowTranslation((prev) => !prev)}
+        onReplay={replaySingleLyric}
+      />
+    </div>
+  );
+}
